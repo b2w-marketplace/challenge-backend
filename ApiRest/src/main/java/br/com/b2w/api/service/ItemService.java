3e8958d097940d6a332b12557ee676c9e85499ee
marcelojava <marcelo.iron09@gmail.com>
2016-11-01 23:02:32 -0200

    organizando o codigo

--- ApiRest/src/main/java/br/com/b2w/api/service/ItemService.java
+++ ApiRest/src/main/java/br/com/b2w/api/service/ItemService.java
@@ -3,7 +3,6 @@ package br.com.b2w.api.service;
 import static java.util.stream.Collectors.toList;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import org.springframework.core.ParameterizedTypeReference;
@@ -14,30 +13,27 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import br.com.b2w.api.model.Item;
+import br.com.b2w.api.util.Util;
 
 @Service(value = "itemService")
 @Component
 public class ItemService {
 
+	private static final String URL_BASE = "http://www.mocky.io/v2/5817803a1000007d01cc7fc9";
+
 	public List<Item> getItens(String dataInit, String dataFim) {
+		return getItensAndFilter(Util.parse(dataInit), Util.parse(dataFim));
+	}
 
-		LocalDate dateInit = parse(dataInit);
-		LocalDate dateFim = parse(dataFim);
-
+	private List<Item> getItensAndFilter(LocalDate dateInit, LocalDate dateFim) {
 		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<List<Item>> itemResponse = restTemplate.exchange(
-				"http://www.mocky.io/v2/5817803a1000007d01cc7fc9", HttpMethod.GET, null,
+		ResponseEntity<List<Item>> itemResponse = restTemplate.exchange(URL_BASE, HttpMethod.GET, null,
 				new ParameterizedTypeReference<List<Item>>() {
 				});
-
+		
 		return itemResponse.getBody().stream()
 				.filter(item -> (item.getDate().isAfter(dateInit) || item.getDate().equals(dateInit))
 						&& (item.getDate().isBefore(dateFim) || item.getDate().equals(dateFim)))
 				.collect(toList());
-	}
-
-	private LocalDate parse(String data) {
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-		return LocalDate.parse(data, formatter);
 	}
 }
