04a1e93eb0a862c0e8cbf57c197ae623d4b30071
Rafael Felix de Moraes <p000rmoraes@prservicos.com.br>
2019-03-18 16:55:41 -0300

    - Melhorias e atualizações do projeto

--- b2w-challenge/src/main/java/com/rafaelfelix/b2wchallenge/controller/exception/ControllerExceptionHandler.java
+++ b2w-challenge/src/main/java/com/rafaelfelix/b2wchallenge/controller/exception/ControllerExceptionHandler.java
@@ -3,6 +3,8 @@ package com.rafaelfelix.b2wchallenge.controller.exception;
 import java.net.UnknownHostException;
 import java.util.Date;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,9 +15,12 @@ import org.springframework.web.client.HttpServerErrorException;
 @ControllerAdvice
 public class ControllerExceptionHandler {
 	
+	private static final Logger LOGGER = LoggerFactory.getLogger(ControllerExceptionHandler.class);
+	
 	@ExceptionHandler(HttpServerErrorException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     protected ResponseEntity<?> httpServerError(HttpServerErrorException exception) {
+		LOGGER.error("Detalhe do erro: ".concat(exception.getMessage()));
 		return ResponseEntity.badRequest()
         	   .body(new StandardError(HttpStatus.BAD_REQUEST.value(), exception.getMessage(), new Date()));
     }
@@ -23,6 +28,7 @@ public class ControllerExceptionHandler {
 	@ExceptionHandler(UnknownHostException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     protected ResponseEntity<?> unknownHost(UnknownHostException exception) {
+		LOGGER.error("Detalhe do erro: ".concat(exception.getMessage()));
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
         	   .body(new StandardError(HttpStatus.NOT_FOUND.value(), "URL não encontrada: ".concat(exception.getMessage()), new Date()));
     }
