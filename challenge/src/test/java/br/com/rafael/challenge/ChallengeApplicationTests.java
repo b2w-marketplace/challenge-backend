db990c2a96a2298b7d88e86d4e99f7edf4cbb432
Rafael Araujo de Souza <r.araujo1915@gmail.com>
2019-03-25 23:25:58 -0300

    Adding some tests

---
+++ challenge/src/test/java/br/com/rafael/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,19 @@
+package br.com.rafael.challenge;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChallengeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+
+
+	}
+
+}
