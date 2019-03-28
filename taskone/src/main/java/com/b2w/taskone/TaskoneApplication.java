84a7f3c16b3a51d7e5386c12969784f0b536be92
Paulo Henrique de Siqueira <paulohenriqu@hotmail.com>
2019-03-28 15:51:42 -0300

    [PHS] - esqueleto da aplicação com Spring Boot

---
+++ taskone/src/main/java/com/b2w/taskone/TaskoneApplication.java
@@ -0,0 +1,13 @@
+package com.b2w.taskone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskoneApplication.class, args);
+	}
+
+}
---
+++ taskone/src/test/java/com/b2w/taskone/TaskoneApplicationTests.java
@@ -0,0 +1,16 @@
+package com.b2w.taskone;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaskoneApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
