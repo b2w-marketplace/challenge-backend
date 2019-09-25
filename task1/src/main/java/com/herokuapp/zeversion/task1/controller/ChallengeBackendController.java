42014c3c30326d084f66e68c345db443ef887b38
Everson <everson___@hotmail.com>
2019-09-24 23:36:36 -0300

    add Service

--- task1/src/main/java/com/herokuapp/zeversion/task1/controller/ChallengeBackendController.java
+++ task1/src/main/java/com/herokuapp/zeversion/task1/controller/ChallengeBackendController.java
@@ -1,5 +1,7 @@
 package com.herokuapp.zeversion.task1.controller;
 
+import com.herokuapp.zeversion.task1.service.ChallengeBackendService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,9 +13,16 @@ import java.util.List;
 @RestController
 public class ChallengeBackendController {
 
+    private final ChallengeBackendService challengeBackendService;
+
+    @Autowired
+    public ChallengeBackendController(ChallengeBackendService challengeBackendService) {
+        this.challengeBackendService = challengeBackendService;
+    }
+
     @GetMapping("/challenge-backend/item")
     public List<?> getItens(@RequestParam("begindate") @DateTimeFormat(pattern = "dd-MM-yyyy") LocalDate begindate,
                             @RequestParam("finaldate") @DateTimeFormat(pattern = "dd-MM-yyyy") LocalDate finaldate) {
-        return null;
+        return this.challengeBackendService.getItens(begindate, finaldate);
     }
 }
---
+++ task1/src/main/java/com/herokuapp/zeversion/task1/service/ChallengeBackendService.java
@@ -0,0 +1,14 @@
+package com.herokuapp.zeversion.task1.service;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class ChallengeBackendService {
+
+    public List<?> getItens(LocalDate begindate, LocalDate finaldate) {
+        return null;
+    }
+}
