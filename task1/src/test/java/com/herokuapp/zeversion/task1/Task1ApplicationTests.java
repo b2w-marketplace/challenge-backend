3703faafca0fcaf2e839792976961a01076f748f
Everson <everson___@hotmail.com>
2019-09-24 23:16:10 -0300

    first commit

---
+++ task1/src/test/java/com/herokuapp/zeversion/task1/Task1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.herokuapp.zeversion.task1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Task1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
