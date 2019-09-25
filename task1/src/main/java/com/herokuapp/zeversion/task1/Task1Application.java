c77ae8e146e08632d22c4991fc8f07efc5168cd3
Everson <everson___@hotmail.com>
2019-09-25 00:14:21 -0300

    Enable Feign Clients

--- task1/src/main/java/com/herokuapp/zeversion/task1/Task1Application.java
+++ task1/src/main/java/com/herokuapp/zeversion/task1/Task1Application.java
@@ -2,8 +2,10 @@ package com.herokuapp.zeversion.task1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class Task1Application {
 
 	public static void main(String[] args) {
