338476ad4b47b2a07977306af73c999282ec75a0
Everson <everson___@hotmail.com>
2019-09-25 00:16:11 -0300

    filter Order API by data sent

--- task1/src/main/java/com/herokuapp/zeversion/task1/service/ChallengeBackendService.java
+++ task1/src/main/java/com/herokuapp/zeversion/task1/service/ChallengeBackendService.java
@@ -1,14 +1,30 @@
 package com.herokuapp.zeversion.task1.service;
 
+import com.herokuapp.zeversion.task1.client.OrderClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ChallengeBackendService {
 
+    private final OrderClient orderClient;
+
+    @Autowired
+    public ChallengeBackendService(OrderClient orderClient) {
+        this.orderClient = orderClient;
+    }
+
     public List<?> getItens(LocalDate begindate, LocalDate finaldate) {
-        return null;
+        return this.orderClient.getItens().stream()
+                .filter(item -> isBetween(begindate, finaldate, item.getDate().toLocalDate()))
+                .collect(Collectors.toList());
+    }
+
+    private boolean isBetween(LocalDate begindate, LocalDate finaldate, LocalDate date) {
+        return date.isAfter(begindate.minusDays(1)) && date.isBefore(finaldate.plusDays(1));
     }
 }
