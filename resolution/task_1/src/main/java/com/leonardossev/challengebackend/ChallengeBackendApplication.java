dff666f7ed4d87c627e3c1c30a11c025beda078b
leonardossev <leonardossev@gmail.com>
2020-12-16 17:14:14 -0300

    feature(project): Adds the Spring Boot project for task 1

---
+++ resolution/task_1/src/main/java/com/leonardossev/challengebackend/ChallengeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.leonardossev.challengebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeBackendApplication.class, args);
+	}
+
+}
