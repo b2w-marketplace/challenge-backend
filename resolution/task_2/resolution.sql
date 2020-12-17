493fb0059fcbcdddda3e4f0c2476e5d1b5641eea
leonardossev <leonardossev@gmail.com>
2020-12-17 10:34:23 -0300

    feature(task2): Adds the task2 resolution

---
+++ resolution/task_2/resolution.sql
@@ -0,0 +1,18 @@
+SELECT 
+	event_type,
+	(
+		(SELECT
+			value
+		FROM events
+		WHERE events.event_type = outer_events.event_type
+		ORDER BY event_type DESC, time DESC LIMIT 1, 1)
+    - 
+		(SELECT
+			value
+		FROM events
+		WHERE events.event_type = outer_events.event_type
+		ORDER BY event_type, time LIMIT 1)
+    ) AS value
+FROM events outer_events
+GROUP BY event_type
+HAVING count(*) > 1;
