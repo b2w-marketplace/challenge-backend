bf2857a84cd51ae7942bf8bb95b44263371b6804
leonardossev <leonardossev@gmail.com>
2020-12-16 21:46:36 -0300

    config(application): Defines port and feign configuration

--- resolution/task_1/src/main/java/com/leonardossev/challengebackend/ChallengeBackendApplication.java
+++ resolution/task_1/src/main/java/com/leonardossev/challengebackend/ChallengeBackendApplication.java
@@ -2,8 +2,10 @@ package com.leonardossev.challengebackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ChallengeBackendApplication {
 
 	public static void main(String[] args) {
