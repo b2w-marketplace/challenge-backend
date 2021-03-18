a83db40e9ac5ef3d2160f47ac79e0758a1ce1dcb
andrefalvesp <andre_falvesp@hotmail.com>
2021-03-17 21:18:12 -0300

    [Initial Commit]

---
+++ task1/src/test/java/com/andrefalvesp/challengebackend/ChallengeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrefalvesp.challengebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
